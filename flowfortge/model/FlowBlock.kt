56a32660e13e5b8b1ac4912b716274ea46410613
Codex96 <83451111+ByteSized96@users.noreply.github.com>
2026-04-25 15:25:00 +0100

    Update FlowBlock.kt

--- flowfortge/model/FlowBlock.kt
+++ flowfortge/model/FlowBlock.kt
@@ -1,1 +1,16 @@
 
+package com.flowforge.model
+
+data class FlowBlock(
+    val id: Long,
+    val title: String,
+    val description: String,
+    val type: FlowBlockType,
+    val config: MutableMap<String, String> = mutableMapOf()
+)
+
+enum class FlowBlockType {
+    TRIGGER,
+    ACTION,
+    CONDITION
+}
